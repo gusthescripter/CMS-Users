16dd13b865061f4ca0e609de2cef9edf94151e1b
gusthescripter <gus.spencer@protonmail.com>
2021-07-08 18:12:33 -0700

    fixed form functions

--- public/class-cms-users-public.php
+++ public/class-cms-users-public.php
@@ -60,25 +60,32 @@ class CMS_Users_Public {
 	public function reg_form() {
 
     $first_name = ( ! empty( $_POST['first_name'] ) ) ? sanitize_text_field( $_POST['first_name'] ) : '';
+	$last_name = ( ! empty( $_POST['last_name'] ) ) ? sanitize_text_field( $_POST['last_name'] ) : '';
         
         ?>
         <p>
             <label for="first_name"><?php _e( 'First Name', 'mydomain' ) ?><br />
                 <input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr(  $first_name  ); ?>" size="25" /></label>
         </p>
-        <p>
+
+		<p>
             <label for="last_name"><?php _e( 'Last Name', 'mydomain' ) ?><br />
                 <input type="text" name="last_name" id="last_name" class="input" value="<?php echo esc_attr(  $last_name  ); ?>" size="25" /></label>
         </p>
+
         <?php
     }
 
     //2. Add validation. In this case, we make sure first_name is required.
     
-    public function reg_err( $errors, $sanitized_user_login, $user_email ) {
+    public function reg_err( $errors ) {
         
         if ( empty( $_POST['first_name'] ) || ! empty( $_POST['first_name'] ) && trim( $_POST['first_name'] ) == '' ) {
         $errors->add( 'first_name_error', sprintf('<strong>%s</strong>: %s',__( 'ERROR', 'mydomain' ),__( 'You must include a first name.', 'mydomain' ) ) );
+
+        }
+		if ( empty( $_POST['last_name'] ) || ! empty( $_POST['last_name'] ) && trim( $_POST['last_name'] ) == '' ) {
+        $errors->add( 'last_name_error', sprintf('<strong>%s</strong>: %s',__( 'ERROR', 'mydomain' ),__( 'You must include a last name.', 'mydomain' ) ) );
 
         }
 
@@ -91,6 +98,10 @@ class CMS_Users_Public {
         if ( ! empty( $_POST['first_name'] ) ) {
             update_user_meta( $user_id, 'first_name', sanitize_text_field( $_POST['first_name'] ) );
         }
+		if ( ! empty( $_POST['last_name'] ) ) {
+            update_user_meta( $user_id, 'last_name', sanitize_text_field( $_POST['last_name'] ) );
+        }
+		
     }
 	
 	
